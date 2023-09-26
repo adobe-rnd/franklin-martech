7ae0ff9d4de292a97abdd3abe71395cbfc1420b9
ekdogan <ekdogan@adobe.com>
2023-09-26 12:45:53 +0200

    tracker webworkerized

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,7 +10,7 @@ import {
   decorateTemplateAndTheme,
   waitForLCP,
   loadBlocks,
-  loadCSS,
+  loadCSS, loadScript,
 } from './lib-franklin.js';
 
 const LCP_BLOCKS = []; // add your LCP blocks to the list
@@ -113,6 +113,13 @@ async function loadEager(doc) {
   }
 }
 
+function initPartytown() {
+  window.partytown = {
+    lib: '/scripts/partytown/',
+  };
+  import('./partytown/partytown.js');
+}
+
 /**
  * Loads everything that doesn't need to be delayed.
  * @param {Element} doc The container element
@@ -134,6 +141,12 @@ async function loadLazy(doc) {
   sampleRUM('lazy');
   sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
   sampleRUM.observe(main.querySelectorAll('picture > img'));
+
+  loadScript('https://cdn.jsdelivr.net/gh/ekremney/acme-tracker/tracker.js', {
+    type: 'text/partytown',
+  });
+
+  initPartytown();
 }
 
 /**
