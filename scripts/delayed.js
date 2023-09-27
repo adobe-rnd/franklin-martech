8276ed6a1f18e473dd59879943f413808888b876
ekdogan <ekdogan@adobe.com>
2023-09-27 09:57:16 +0200

    acme tracker integration delayed

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,8 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './lib-franklin.js';
+import { loadScript, sampleRUM } from './lib-franklin.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
 // add more delayed functionality here
+loadScript('https://cdn.jsdelivr.net/gh/ekremney/acme-tracker/tracker.js');
